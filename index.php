38d087a1f419f3bc4b8da260b154841527c2c213
zaldyauw <34432396+zaldyauw@users.noreply.github.com>
2019-10-03 19:20:23 +0700

    Initial Commit

--- index.php
+++ index.php
@@ -1,87 +1,155 @@
+<?php
+session_start();
+include_once 'db_function/insurance_func.php';
+include_once 'db_function/patient_func.php';
+include_once 'db_function/user_func.php';
+include_once 'db_function/db_helper.php';
+include_once 'view/view_util.php';
+
+if(!isset($_SESSION['user_logged'])) {
+    $_SESSION['user_logged'] = false;
+}
+
+
+
+?>
+
 <!DOCTYPE html>
+
 <html>
 <head>
-    <title>Review PWL</title>
-    <META CHARSET="UTF-8">
-    <META LANG="EN">
-    <meta name="author" content="zaldy">
-    <meta name="description" content="web,php">
-    <link rel="stylesheet" href="plugins/datatables.min.css">
-    <link rel="stylesheet" href="plugins/hehe.css">
-    <script type="text/javascript" src="plugins/datatables.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/tableStyle.css">
+    <script src="js/my_js.js"></script>
 </head>
+
 <body>
-<script language="JavaScript">
-    function hitung() {
-        var myForm = document.form1;
-        var x=eval(myForm.x.value);
-        var y=eval(myForm.y.value);
-        var pil= myForm.opt.value;
-        if (pil == "tambah") {
-            var z = x + y;
-        }else if (pil == "kurang") {
-            var z = x - y;
-        } else if (pil == "kali") {
-            var z = x * y;
-        } else if (pil == "bagi") {
-            var z = x / y;
-        }
-        myForm.hasil.value = z;
-        myForm.x.value = "";
-        myForm.y.value = "";
-    }
-</script>
-<table id="tableSaya">
-    <thead>
-        <tr>
-            <th>haii</th>
-            <th>haii</th>
-            <th>haii</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>hi!</td>
-            <td>hi!</td>
-            <td>hi!</td>
-        </tr>
-        <tr>
-            <td>hi!</td>
-            <td>hi!</td>
-            <td>hi!</td>
-        </tr>
-        <tr>
-            <td>hi!</td>
-            <td>hi!</td>
-            <td>hi!</td>
-        </tr>
-    </tbody>
-</table>
-<div align="center">
-    <form name="form1" action="#">
-        <p>bil 1  <input type="text" name="x" /></p>
-        <p>bil 2  <input type="text" name="y" /></p>
-        <p>hasil  <input type="text" /></p>
-        <p>operator <select name="opt"></p>
-        <option value="tambah"> + (tambah)</option>
-        <option value="kurang"> - (kurang)</option>
-        <option value="kali"> * (kali)</option>
-        <option value="bagi"> / (bagi)</option>
-        </select>
-        <input type="button" value="=" onClick="hitung()" />
-    </form>
+<div class="page">
     <?php
-     $angka1 = 25;
-     $angka2 = 3.14;
-     $isStudent =  true;
-     if($angka1 >= 20){
-         echo "angka lebih besar dari 20";
-     }
+
+    if($_SESSION['user_logged']) {?>
+        <h4><?php echo "Selamat Datang " . $_SESSION['name']?></h4><hr>
+
+   <?php if ($_SESSION['role'] == 1) {
     ?>
-<script>
-    $(document).ready( function () {
-        $('#tableSaya').DataTable();
-    } );
-</script>
-</body>
+    <h4>Menu</h4>
+
+
+    <nav>
+        <ul>
+            <li><a href="?menu=hm">Home</a></li>
+            <li><a href="?menu=pt">Patient</a></li>
+            <li><a href="?menu=ins">Insurance</a></li>
+            <li><a href="?menu=out">Logout</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <?php
+        $targetMenu = filter_input(INPUT_GET, 'menu');
+        switch ($targetMenu) {
+            case 'hm':
+                include_once 'view/home.php';
+                break;
+            case 'pt':
+                include_once 'view/patient.php';
+                break;
+            case 'ins':
+                include_once 'view/insurance.php';
+                break;
+            case 'updIns':
+                include_once 'view/insurance_update.php';
+                break;
+            case 'updPat':
+                include_once 'view/patient_update.php';
+                break;
+            case 'updUser':
+                include_once 'view/user_update.php';
+                break;
+            case 'out':
+                session_destroy();
+                header("location:index.php");
+                break;
+            default ;
+        }
+        ?>
+    </main>
+
+
+        <?php
+        } elseif ($_SESSION['role'] == 2) {
+        ?>
+        <nav>
+            <ul>
+                <li><a href="?menu=pt">Patient</a></li>
+                <li><a href="?menu=ins">Insurance</a></li>
+                <li><a href="?menu=out">Logout</a></li>
+            </ul>
+        </nav>
+        <main>
+        <?php
+        $targetMenu = filter_input(INPUT_GET, 'menu');
+        switch ($targetMenu) {
+            case 'pt':
+                include_once 'view/patient.php';
+                break;
+            case 'ins':
+                include_once 'view/insurance.php';
+                break;
+            case 'updPat':
+                include_once 'view/patient_update.php';
+                break;
+            case 'updIns':
+                include_once 'view/insurance_update.php';
+                break;
+            case 'out':
+                session_destroy();
+                header("location:index.php");
+            default;
+        }
+        ?>
+        </main>
+        <?php
+        } elseif ($_SESSION['role'] == 3) {?>
+        <nav>
+            <ul>
+                <li><a href="?menu=pt">Patient</a></li>
+                <li><a href="?menu=out">Logout</a></li>
+            </ul>
+        </nav>
+        <main>
+        <?php
+        $targetMenu = filter_input(INPUT_GET, 'menu');
+        switch ($targetMenu) {
+            case 'pt':
+                include_once 'view/patient.php';
+                break;
+            case 'updPat':
+                include_once 'view/patient_update.php';
+                break;
+            case 'out':
+                session_destroy();
+                header("location:index.php");
+            default;
+        }
+        ?>
+        </main>
+        <?php
+        }
+        ?>
+        <?php
+        } else {
+            include_once 'view/login.php';
+        }
+        ?>
+        </div>
+        </body>
+
+        <script>
+        $(document).ready(function () {
+            $('#patient').DataTable();
+            $('#insurance').DataTable();
+            $('#user').DataTable();
+        });
+    </script>
+
 </html>
